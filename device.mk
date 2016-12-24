19884015f9ce70529bc7caac925bc13348bd4ef5
Seraph08 <davidvandenburgh@linux.com>
2016-12-24 06:09:16 -0500

    Revert "for now"
    This reverts commit 6a46a37a552a1a325710073557db78f0009be51a.

--- device.mk
+++ device.mk
@@ -196,11 +196,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
-# OMS
-PRODUCT_PACKAGES += \
-    masquerade \
-    substratum
- 
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
