c09128dde49d576d1061ec8a3e61f38d491d7043
KilluaHatake <chinturocks4u@gmail.com>
2017-01-01 20:56:54 +0530

    Update reaper.mk

--- reaper.mk
+++ reaper.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus2/device.mk)
 
 # Inherit some common reaper stuff.
-$(call inherit-product, vendor/reaper/config/common_full_phone.mk)
-$(call inherit-product, vendor/reaper/common.mk)
+#$(call inherit-product, vendor/reaper/config/common_full_phone.mk)
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
-PRODUCT_NAME := reaper_oneplus2
+PRODUCT_NAME := nitrogen_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
